fe30548fa97de31a4c6defe06152eae18fb1869f
Райхан <raihan.5555@mail.ru>
2023-12-23 20:47:08 +0600

    task1

--- js/script.js
+++ js/script.js
@@ -28,3 +28,18 @@ months.push(person);
 console.log(months);
 
 console.log(months.includes('September'));
+
+function task1(){
+    let n = parseInt(prompt("Введите количество чисел (n):"));
+    let numbers = [];
+    for (let i = 0; i < n; i++) {
+        numbers.push(parseInt(prompt('insert number')));
+    }
+    let m = parseInt(prompt("Введите количество число (m):"));
+    if (numbers.includes(m)){
+        alert("YES");
+    } else {
+        alert('NO');
+    }
+}
+task1();
